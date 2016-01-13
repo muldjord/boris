a209c31a88d50fb185b92240bf02ed6f3c583f73
Lars Muldjord <larsbisballe@gmail.com>
2016-01-14 00:34:24 +0100

    Now flees instead of waving if other Boris is dirty

--- boris.cpp
+++ boris.cpp
@@ -865,49 +865,49 @@ void Boris::collide(Boris *b)
     }
     
     if((socialAngle >= 0.0 && socialAngle < 22.5) || (socialAngle >= 337.5 && socialAngle < 360.0)) {
-      if(boris->getHygiene() >= 90) {
+      if(boris->getHygiene() >= 15) {
         changeBehaviour("_casual_wave_right");
       } else {
         changeBehaviour("_flee_left");
       }
     } else if(socialAngle >= 22.5 && socialAngle < 67.5) {
-      if(boris->getHygiene() >= 90) {
+      if(boris->getHygiene() >= 15) {
         changeBehaviour("_casual_wave_right_up");
       } else {
         changeBehaviour("_flee_left_down");
       }
     } else if(socialAngle >= 67.5 && socialAngle < 112.5) {
-      if(boris->getHygiene() >= 90) {
+      if(boris->getHygiene() >= 15) {
         changeBehaviour("_casual_wave_up");
       } else {
         changeBehaviour("_flee_down");
       }
     } else if(socialAngle >= 112.5 && socialAngle < 157.5) {
-      if(boris->getHygiene() >= 90) {
+      if(boris->getHygiene() >= 15) {
         changeBehaviour("_casual_wave_left_up");
       } else {
         changeBehaviour("_flee_right_down");
       }
     } else if(socialAngle >= 157.5 && socialAngle < 202.5) {
-      if(boris->getHygiene() >= 90) {
+      if(boris->getHygiene() >= 15) {
         changeBehaviour("_casual_wave_left");
       } else {
         changeBehaviour("_flee_right");
       }
     } else if(socialAngle >= 202.5 && socialAngle < 247.5) {
-      if(boris->getHygiene() >= 90) {
+      if(boris->getHygiene() >= 15) {
         changeBehaviour("_casual_wave_left_down");
       } else {
         changeBehaviour("_flee_right_up");
       }
     } else if(socialAngle >= 247.5 && socialAngle < 292.5) {
-      if(boris->getHygiene() >= 90) {
+      if(boris->getHygiene() >= 15) {
         changeBehaviour("_casual_wave_down");
       } else {
         changeBehaviour("_flee_up");
       }
     } else if(socialAngle >= 292.5 && socialAngle < 337.5) {
-      if(boris->getHygiene() >= 90) {
+      if(boris->getHygiene() >= 15) {
         changeBehaviour("_casual_wave_right_down");
       } else {
         changeBehaviour("_flee_left_up");
