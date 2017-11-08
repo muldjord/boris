5044029b04f8ceeb6ed461908a64ac317446c8ec
Lars Muldjord <muldjordlars@gmail.com>
2017-11-08 22:31:58 +0100

    Updated translations

--- boris_da_DK.ts
+++ boris_da_DK.ts
@@ -4,94 +4,6 @@
 <context>
     <name>About</name>
     <message>
-        <source>Merry Christmas! You just installed Boris on your desktop. He is here to keep you company during the cold days of december. He is a bit of a prankster, so keep an eye out for him!
-
-Functions:
-- You can move Boris around your desktop by left-clicking and dragging him.
-- Right-clicking Boris or the tray icon brings up a menu:
- - About: Show this about box.
-  - Quit: Tell Boris to leave you alone
-
-If your friends / colleagues become envious and would like a Boris of their own, simply ask them to visit http://www.santacandle.dk and download it. You can also download the full source code in there if you like. This software is COMPLETELY FREE and OPEN SOURCE (GPLv3).</source>
-        <oldsource>Merry Christmas! You just installed Boris on your desktop. He is here to keep you company during the cold days of december. He is a bit of a prankster, so keep an eye out for him!
-
-Functions:
-- You can move Boris around your desktop by left-clicking and dragging him.
-- Right-clicking Boris or the tray icon brings up a menu:
-  - Bring to front: Tell Boris to appear.
-  - About: Show this about box.
-  - Quit: Tell Boris to leave you alone
-
-If your friends / colleagues become envious and would like a Boris of their own, simply ask them to visit http://www.santacandle.dk and download it. You can also download the full source code in there if you like. This software is COMPLETELY FREE and OPEN SOURCE (GPLv3).</oldsource>
-        <translation type="vanished">Glædelig jul! Du har netop installeret Boris på dit skrivebord. Han er klar til at holde dig med selskab gennem de kolde december dage. Han er lidt af en slambert, så hold godt øje med ham!
-
-Funktioner:
-- Du kan flytte Boris ved at venstre-klikke og trække.
-- Med højreklik på enten Boris eller system ikonet får du en menu:
-  - Om: Vis denne boks.
-  - Quit: Bed Boris om at lade dig være.
-
-Hvis dine venner / kollegaer er misundelige og vil have deres egen Boris, så bed dem besøge http://www.santacandle.dk. Her kan de hente deres helt egen version af Boris kvit og frit. Du kan også hente den fulde kildekode derinde. Koden er GPLv3.</translation>
-    </message>
-    <message>
-        <source>Programming: Lars Muldjord
-Graphics: Lars Muldjord
-
-Boris was programmed using the Qt framework (http://www.qt-project.org) and C++.
-
-Bug reports, suggestions and / or comments can be emailed to me at:
-muldjordlars@gmail.com.
-
-If your friends / colleagues would like to get their own Boris, tell them to visit:
-
-http://www.santacandle.dk
-
-It&apos;s FREE and OPEN SOURCE (GPLv3), so there&apos;s absolutely no strings attached, just download away. You can also download the full source code in there if you like.
-
-Copyright 2012 Lars Muldjord. This software is distributed under the terms of the GNU General Public License. Be sure to read the license in the &apos;License&apos; tab or check out the web page http://www.gnu.org/licenses/gpl-3.0.html.</source>
-        <translation type="vanished">Programmering: Lars Muldjord
-Grafik: Lars Muldjord
-
-Boris er programmeret i Qt frameworket (http://www.qt-project.org) og C++.
-
-Fejl rapportering og forslag kan mailes til mig på:
-muldjordlars@gmail.com
-
-Hvis dine venner eller kollegaer vil have deres egen Boris, så bed dem besøge:
-http://www.santacandle.dk
-
-Boris er gratis og open source (GPLv3). Kildekoden kan ligeledes hentes fra hjemmesiden.
-
-Copyright 2012 Lars Muldjord. This software is distributed under the terms of the GNU General Public License. Be sure to read the license in the &apos;License&apos; tab or check out the web page http://www.gnu.org/licenses/gpl-3.0.html.</translation>
-    </message>
-    <message>
-        <source>Boris bids you welcome! He is here to keep you company during all seasons of the year. He is a bit of a prankster, so keep an eye out for him!
-
-Functions:
-- You can throw Boris around your desktop by left-clicking him.
-- Right-clicking Boris brings up a menu where you can order him to do certain things.
-- Right-click the tray icon to either quit, cause an earthquake, move all clones at the same time or open this config/about box.
-
-If your friends / colleagues become envious and would like a Boris of their own, simply ask them to visit http://www.muldjord.com/boris and download it. You can also download the full source code in there if you like. This software is COMPLETELY FREE and OPEN SOURCE (GPLv3).</source>
-        <oldsource>Boris bids you welcome! He is here to keep you company during all seasons of the year. He is a bit of a prankster, so keep an eye out for him!
-
-Functions:
-- You can throw Boris around your desktop by left-clicking him.
-- When the taskbar icon is active (just click it), you can control all clones with the cursor keys.
-- Right-clicking Boris brings up a menu where you can order him to do certain things.
-- Right-click the tray icon to either quit, cause an earthquake or show this about box.
-
-If your friends / colleagues become envious and would like a Boris of their own, simply ask them to visit http://www.muldjord.com/boris and download it. You can also download the full source code in there if you like. This software is COMPLETELY FREE and OPEN SOURCE (GPLv3).</oldsource>
-        <translation type="vanished">Boris byder dig velkommen! Han er her for at underholde dig gennem alle årets årstider. Han er lidt af en baryler, så hold godt øje med ham!
-
-Funktioner:
-- Du kan smide Boris rundt på skrivebordet ved at venstre-klikke.
-- Højreklik på Boris frembringer en menu hvor du kan vælge hvad han skal lave.
-- Højreklik tray ikonet for at quitte, forårsage et jordskælv, flytte all kloner på én gang eller vise dette vindue.
-
-Hvis dine venner / kollegaer er misundelige og vil have deres egen Boris, så bed dem besøge http://www.muldjord.com/boris. Her kan de hente deres helt egen version af Boris kvit og frit. Du kan også hente den fulde kildekode derinde. Koden er GPLv3.</translation>
-    </message>
-    <message>
         <location filename="about.cpp" line="53"/>
         <source>Boris bids you welcome! He is here to keep you company during all seasons of the year. He is a bit of a prankster, so keep an eye out for him!
 
@@ -207,10 +119,6 @@ Copyright 2016 Lars Muldjord. This software is distributed under the terms of th
         <translation>Licens</translation>
     </message>
     <message>
-        <source>Ok</source>
-        <translation type="vanished">Ok</translation>
-    </message>
-    <message>
         <location filename="about.cpp" line="113"/>
         <source>Boris size in pixels (32-256 or 0 for random):</source>
         <oldsource>Boris size in pixels (32-256):</oldsource>
@@ -250,27 +158,37 @@ Copyright 2016 Lars Muldjord. This software is distributed under the terms of th
         <translation>Vejr funktionaliteten skal bruge en API nøgle for at fungere. Standard nøglen burde fungere.&lt;br/&gt;I tilfælde af at den ikke gør, kan du få en nye på openweathermap.org/appid</translation>
     </message>
     <message>
-        <location filename="about.cpp" line="153"/>
+        <location filename="about.cpp" line="148"/>
+        <source>RSS feed url:</source>
+        <translation>RSS feed adresse:</translation>
+    </message>
+    <message>
+        <location filename="about.cpp" line="150"/>
+        <source>Type in any RSS feed url. Boris will sometimes update you on a title from this feed</source>
+        <translation>Indtast en vilkårlig RSS feed adresse. Boris vil nogen gange fortælle dig en titel fra dette feed</translation>
+    </message>
+    <message>
+        <location filename="about.cpp" line="160"/>
         <source>Independence:</source>
         <translation>Selvstændighed:</translation>
     </message>
     <message>
-        <location filename="about.cpp" line="161"/>
+        <location filename="about.cpp" line="168"/>
         <source>Enable Boris speech bubbles</source>
         <translation>Tillad Boris talebobler</translation>
     </message>
     <message>
-        <location filename="about.cpp" line="166"/>
+        <location filename="about.cpp" line="173"/>
         <source>Enable sound</source>
         <translation>Aktivér lyd</translation>
     </message>
     <message>
-        <location filename="about.cpp" line="179"/>
+        <location filename="about.cpp" line="186"/>
         <source>Always show this dialog on startup</source>
         <translation>Vis altid denne dialog ved start</translation>
     </message>
     <message>
-        <location filename="about.cpp" line="148"/>
+        <location filename="about.cpp" line="155"/>
         <source>Always show vitality stats</source>
         <translation>Vis altid Boris&apos; helbred</translation>
     </message>
@@ -280,63 +198,55 @@ Copyright 2016 Lars Muldjord. This software is distributed under the terms of th
         <translation>Luk og gem</translation>
     </message>
     <message>
-        <location filename="about.cpp" line="171"/>
+        <location filename="about.cpp" line="178"/>
         <source>Sound volume:</source>
         <translation>Lyd niveau:</translation>
-    </message>
-    <message>
-        <source>Show this on startup</source>
-        <translation type="vanished">Vis dette vindue ved startup</translation>
     </message>
 </context>
 <context>
     <name>Boris</name>
     <message>
-        <location filename="boris.cpp" line="168"/>
+        <location filename="boris.cpp" line="170"/>
         <source>Behaviours</source>
         <translation>Opførsler</translation>
     </message>
     <message>
-        <location filename="boris.cpp" line="169"/>
+        <location filename="boris.cpp" line="171"/>
         <source>Health</source>
         <translation>Helbred</translation>
     </message>
     <message>
-        <location filename="boris.cpp" line="183"/>
+        <location filename="boris.cpp" line="185"/>
         <source>Movement</source>
         <translation>Bevægelse</translation>
     </message>
     <message>
-        <location filename="boris.cpp" line="171"/>
+        <location filename="boris.cpp" line="173"/>
         <source>Energy</source>
         <translation>Energi</translation>
     </message>
     <message>
-        <source>Hunger</source>
-        <translation type="vanished">Sult</translation>
-    </message>
-    <message>
-        <location filename="boris.cpp" line="173"/>
+        <location filename="boris.cpp" line="175"/>
         <source>Food</source>
         <translation>Mad</translation>
     </message>
     <message>
-        <location filename="boris.cpp" line="175"/>
+        <location filename="boris.cpp" line="177"/>
         <source>Toilet</source>
         <translation>Toilet</translation>
     </message>
     <message>
-        <location filename="boris.cpp" line="179"/>
+        <location filename="boris.cpp" line="181"/>
         <source>Social</source>
         <translation>Social</translation>
     </message>
     <message>
-        <location filename="boris.cpp" line="181"/>
+        <location filename="boris.cpp" line="183"/>
         <source>Fun</source>
         <translation>Sjov</translation>
     </message>
     <message>
-        <location filename="boris.cpp" line="177"/>
+        <location filename="boris.cpp" line="179"/>
         <source>Hygiene</source>
         <translation>Hygiejne</translation>
     </message>
@@ -344,62 +254,38 @@ Copyright 2016 Lars Muldjord. This software is distributed under the terms of th
 <context>
     <name>MainWindow</name>
     <message>
-        <source>&amp;About...</source>
-        <translation type="vanished">&amp;Om...</translation>
-    </message>
-    <message>
-        <source>&amp;Bring to front</source>
-        <translation type="obsolete">&amp;Bring frem</translation>
-    </message>
-    <message>
-        <location filename="mainwindow.cpp" line="171"/>
+        <location filename="mainwindow.cpp" line="175"/>
         <source>&amp;Config / about...</source>
         <translation>&amp;Konfigurér / om...</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="175"/>
+        <location filename="mainwindow.cpp" line="179"/>
         <source>&amp;Earthquake!!!</source>
         <oldsource>&amp;Earthquake</oldsource>
         <translation>&amp;Jordskælv!!!</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="176"/>
+        <location filename="mainwindow.cpp" line="180"/>
         <source>&amp;Beam me up, Scotty!</source>
         <translation>&amp;Beam me up, Scotty!</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="178"/>
+        <location filename="mainwindow.cpp" line="182"/>
         <source>Updating weather...</source>
         <translation>Opdaterer vejr...</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="277"/>
+        <location filename="mainwindow.cpp" line="281"/>
         <source> degrees Celsius</source>
         <translation> grader Celsius</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="279"/>
+        <location filename="mainwindow.cpp" line="283"/>
         <source>Couldn&apos;t find city</source>
         <translation>Kunne ikke finde by</translation>
     </message>
     <message>
-        <source>Walk up</source>
-        <translation type="vanished">Gå opad</translation>
-    </message>
-    <message>
-        <source>Walk down</source>
-        <translation type="vanished">Gå nedad</translation>
-    </message>
-    <message>
-        <source>Walk left</source>
-        <translation type="vanished">Gå mod venstre</translation>
-    </message>
-    <message>
-        <source>Walk right</source>
-        <translation type="vanished">Gå mod højre</translation>
-    </message>
-    <message>
-        <location filename="mainwindow.cpp" line="180"/>
+        <location filename="mainwindow.cpp" line="184"/>
         <source>&amp;Quit</source>
         <translation>&amp;Quit</translation>
     </message>
