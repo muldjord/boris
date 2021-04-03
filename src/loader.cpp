4294e1ee1f434d8f6797d549281d2f17160693ac
Lars Muldjord <muldjordlars@gmail.com>
2021-04-03 14:53:58 +0200

    Now handles æ, ø and å

--- src/loader.cpp
+++ src/loader.cpp
@@ -285,11 +285,11 @@ Script Loader::parseScript(const QString &script)
   return returnScript;
 }
 
-bool Loader::loadFont(QMap<QChar, QImage> &pfont)
+bool Loader::loadFont(QMap<QString, QImage> &pfont)
 {
   QImage fontSheet(":pfont.png");
   if(!fontSheet.isNull()) {
-    QList<QChar> fontChars({'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ', '.', ',', ':', ';', '!', '?', '\"', '\'', '_', '+', '-', '*', '/', '<', '>', '='});
+    QList<QString> fontChars({"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Æ", "Ø", "Å", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "æ", "ø", "å", " ", ".", ",", ":", ";", "!", "?", "\"", "\"", "_", "+", "-", "*", "/", "<", ">", "="});
     int w = fontSheet.width();
     int h = fontSheet.height();
     int x1 = 0;
